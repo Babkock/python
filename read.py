9658d0aa5349d81711a47523584a66394eebe6df
Tanner Babcock <babkock@gmail.com>
2019-08-26 04:32:47 -0500

    Line numbers to read.py

--- read.py
+++ read.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 inF = open("input.txt", "r")
 lines = inF.read().splitlines()
+x = 0
 
 for line in lines:
-    print("{}".format(line))
+    x += 1
+    print("{}: {}".format(x, line))
 
 inF.close()
 
