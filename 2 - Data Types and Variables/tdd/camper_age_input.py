9943b7acedae4a4211f44cf9e60b33bff58ffffa
Tanner Babcock <babkock@gmail.com>
2019-09-10 20:04:27 -0500

    Test driven development

---
+++ 2 - Data Types and Variables/tdd/camper_age_input.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+September 10, 2019
+
+Topic 5: Test-driven development
+"""
+
+def convert_to_months(years):
+    months = (years * 12)
+    return months
+
+if __name__ == "__main__":
+    age_in_years = input("Age in years: ")
+    age_in_months = convert_to_months(int(age_in_years))
+
+    print("{} years is {} months".format(age_in_years, age_in_months))
+
+# Example input: 3
+# Expected output: 3 years is 36 months
+
---
+++ 2 - Data Types and Variables/tdd/test_functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+September 10, 2019
+
+Topic 5: Test-driven development
+"""
+import unittest
+from camper_age_input import convert_to_months
+
+class FunctionTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(convert_to_months(10), 120)
+
+if __name__ == "__main__":
+    unittest.main()
+
