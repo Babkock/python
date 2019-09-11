6ad97f772fb6c009f8e88ade00ced664d75fdd1d
Tanner Babcock <babkock@gmail.com>
2019-09-10 19:19:02 -0500

    Function.py

---
+++ 3 - Strings, Basic IO, Operators/function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+September 10, 2019
+
+Small program demonstrating sample function main
+"""
+def main():
+    print("in main")
+
+
+if __name__ == '__main__':
+    main()
+
+
