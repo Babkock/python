3fa64df0cb6b0add7731d2eab6342146a09afc44
Tanner Babcock <babkock@gmail.com>
2019-09-17 19:24:10 -0500

    Started working on tests for Module 4, Topic 3

--- 4 - Conditional Operators/Nested If/store/coupon_calculations.py
+++ 4 - Conditional Operators/Nested If/store/coupon_calculations.py
@@ -9,35 +9,39 @@ def calculate_order(price, cash_coupon, percent_coupon):
     final_price = price
     discount = 0
     shipping = 0
-    print("Initial price: {}".format(price))
+    # print("Initial price: {}".format(price))
     if cash_coupon > 0:
         final_price -= cash_coupon
-        print("Cash coupon used")
+        # print("Cash coupon used")
         if percent_coupon > 0:
             discount = round(final_price * (percent_coupon * 0.01), 2)
-            print("Percent coupon used")
+            # print("Percent coupon used")
             final_price -= discount
     else:
         if percent_coupon > 0:
             discount = round(final_price * (percent_coupon * 0.01), 2)
-            print("Percent coupon used")
+            # print("Percent coupon used")
             final_price -= discount
     tax = round((final_price * 0.06), 2) # sales tax
-    print("Subtotal: {}".format(final_price))
+    # print("Subtotal: {}".format(final_price))
     if final_price <= 10.00:
-        print("Small shipping")
+        # print("Small shipping")
         shipping = 5.95
     elif (final_price > 10.00) and (final_price <= 30.00):
-        print("Medium shipping")
+        # print("Medium shipping")
         shipping = 7.95
     elif (final_price > 30.00) and (final_price <= 50.00):
-        print("Large shipping")
+        # print("Large shipping")
         shipping = 11.95
     else:
         shipping = 0
     return (final_price + shipping + tax)
 
 if __name__ == '__main__':
-    price = 15.99
-    print("init price {0} = {1:.2f}".format(price, calculate_order(price, 5, 30)));
+    print("7.00, $5 off, 10% off = $ {0:.2f}".format(calculate_order(7.00, 5, 10)))
+    print("7.50, $5 off, 15% off = $ {0:.2f}".format(calculate_order(7.50, 5, 15)))
+    print("7.75, $5 off, 20% off = $ {0:.2f}".format(calculate_order(7.75, 5, 20)))
+    print("8.00, $10 off, 10% off = $ {0:.2f}".format(calculate_order(8.00, 10, 10)))
+    print("8.25, $11 off, 15% off = $ {0:.2f}".format(calculate_order(8.25, 11, 15)))
+    print("9.00, $12 off, 10% off = $ {0:.2f}".format(calculate_order(9.00, 12, 10)))
 
--- 4 - Conditional Operators/Nested If/test/test_coupon_calculations.py
+++ 4 - Conditional Operators/Nested If/test/test_coupon_calculations.py
@@ -10,14 +10,19 @@ from coupon_calculations import calculate_order
 
 class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten(self):
-        
+        self.assertEqual(round(calculate_order(7.00, 5, 10), 2), 7.86)
+        self.assertEqual(round(calculate_order(7.50, 5, 15), 2), 8.2)
+        self.assertEqual(round(calculate_order(7.75, 5, 20), 2), 8.28)
+        self.assertEqual(round(calculate_order(8.00, 10, 10), 2), 4.04)
+        self.assertEqual(round(calculate_order(8.25, 11, 15), 2), 3.47)
+        self.assertEqual(round(calculate_order(9.00, 12, 10), 2), 3.09)
 
-    def test_price_under_between_ten_thirty(self):
+    #def test_price_under_between_ten_thirty(self):
 
 
-    def test_price_under_between_thirty_fifty(self):
+    #def test_price_under_between_thirty_fifty(self):
 
-    def test_price_under_over_fifty(self):
+    #def test_price_under_over_fifty(self):
 
 
 
