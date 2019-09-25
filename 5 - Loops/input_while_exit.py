b8f5bbec60f0f453192f34c8903cff3d661380f1
Tanner Babcock <babkock@gmail.com>
2019-09-24 19:11:15 -0500

    Input_while_exit

---
+++ 5 - Loops/input_while_exit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+September 24, 2019
+
+Module 4, topic 2: The while loop
+"""
+
+if __name__ == '__main__':
+    theList = []
+    go = input("Enter a number, or 'q' to quit: ")
+    while go != 'q':
+        # check if the user typed 'q' or not
+        while go.isdigit() == False:
+            # get input again if it's not really a number
+            go = input("Please enter a number, or 'q' to quit: ")
+            if go == 'q':
+                break
+        if go == 'q':
+            break
+        theList.append(go)
+        # append to theList
+        go = input("Enter a number, or 'q' to quit: ")
+
+    count = 0
+    for iterator in theList:
+        print("{0}: {1}".format(count, iterator))
+        count += 1
+
