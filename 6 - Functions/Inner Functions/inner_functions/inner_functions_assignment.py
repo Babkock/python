e48316a86894d8a507e2a303604c6e0fe6dbbcb2
Tanner Babcock <babkock@gmail.com>
2019-10-01 22:58:16 -0500

    Inner functions tests, tester script

--- 6 - Functions/Inner Functions/inner_functions/inner_functions_assignment.py
+++ 6 - Functions/Inner Functions/inner_functions/inner_functions_assignment.py
@@ -6,10 +6,46 @@ October 1, 2019
 Module 6, topic 5: Inner Functions and Decorators
 """
 
+"""
+This function takes a variable-length list of measurements
+and returns a string giving the perimeter and the area
+
+:param measure_list: A list of measurements, can be at least 1 and up to 2 numbers
+:returns: A string with the calculated area and perimeter
+:raises ValueError: If measure_list has a length of 0, or if measurements are neither ints nor floats
+"""
 def measurements(measure_list):
     def area(a_list):
-        
+        if len(a_list) == 0:
+            raise ValueError
+        elif len(a_list) == 1:
+            if (isinstance(a_list[0], int) != True) and (isinstance(a_list[0], float) != True):
+                raise ValueError
+
+            return float(a_list[0] * a_list[0])
+        else:
+            if (isinstance(a_list[0], int) != True) and (isinstance(a_list[0], float) != True):
+                raise ValueError
+            if (isinstance(a_list[1], int) != True) and (isinstance(a_list[1], float) != True):
+                raise ValueError
+
+            return float(a_list[0] * a_list[1])
+
     def perimeter(a_list):
+        if len(a_list) == 0:
+            raise ValueError
+        elif len(a_list) == 1:
+            if (isinstance(a_list[0], int) != True) and (isinstance(a_list[0], float) != True):
+                raise ValueError
+
+            return float(a_list[0] + a_list[0] + a_list[0] + a_list[0])
+        else:
+            if (isinstance(a_list[0], int) != True) and (isinstance(a_list[0], float) != True):
+                raise ValueError
+            if (isinstance(a_list[1], int) != True) and (isinstance(a_list[1], float) != True):
+                raise ValueError
+
+            return float(a_list[0] + a_list[0] + a_list[1] + a_list[1])
 
     return "Perimeter = {0:.2f}, Area = {1:.02f}".format(perimeter(measure_list), area(measure_list))
 
--- 6 - Functions/Inner Functions/test/test_inner_functions.py
+++ 6 - Functions/Inner Functions/test/test_inner_functions.py
@@ -5,4 +5,16 @@ October 1, 2019
 
 Module 6, topic 5: Inner functions and Decorators
 """
+import unittest
+from inner_functions_assignment import measurements
+
+class InnerFunctionsTest(unittest.TestCase):
+    def test_measurements_rectangle(self):
+        self.assertEqual(measurements([2.1, 3.4]), "Perimeter = 11.00, Area = 7.14")
+
+    def test_measurements_square(self):
+        self.assertEqual(measurements([3.5]), "Perimeter = 14.00, Area = 12.25")
+
+if __name__ == "__main__":
+    unittest.main()
 
