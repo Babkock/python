1bc17f3b92674c057f4688400e7279ba67a66a0b
Tanner Babcock <babkock@gmail.com>
2019-10-01 22:23:19 -0500

    Another commit

---
+++ 6 - Functions/Inner Functions/inner_functions/inner_functions_assignment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+October 1, 2019
+
+Module 6, topic 5: Inner Functions and Decorators
+"""
+
+def measurements(measure_list):
+    def area(a_list):
+        
+    def perimeter(a_list):
+
+    return "Perimeter = {0:.2f}, Area = {1:.02f}".format(perimeter(measure_list), area(measure_list))
+
---
+++ 6 - Functions/Inner Functions/test/test_inner_functions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+October 1, 2019
+
+Module 6, topic 5: Inner functions and Decorators
+"""
+
