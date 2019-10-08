34224f37ed57c045dc4ddfc212b0654c61716b84
Tanner Babcock <babkock@gmail.com>
2019-10-08 17:39:43 -0500

    Imma take it one step further

--- 7 - Collections/Module7/fun_with_collections/basic_list.py
+++ 7 - Collections/Module7/fun_with_collections/basic_list.py
@@ -7,9 +7,18 @@ Module 7, topic 1: Lists
 """
 
 def make_list():
-    pass
+    x = 0
+    theList = [0, 0, 0]
+    for x in range(0, 3):
+        theList[x] = int(input("Enter an int number: "))
+    return theList
 
 def get_input():
     pass
 
-
+if __name__ == "__main__":
+    try:
+        myList = make_list()
+        print("{}, {}, {}".format(myList[0], myList[1], myList[2]))
+    except:
+        print("Bad input")
