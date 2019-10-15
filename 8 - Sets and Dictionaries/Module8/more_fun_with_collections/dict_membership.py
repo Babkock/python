f9e8dc30fc8392404c40c9252ac345131a41fc7f
Tanner Babcock <babkock@gmail.com>
2019-10-15 18:35:16 -0500

    Dictionary tests

--- 8 - Sets and Dictionaries/Module8/more_fun_with_collections/dict_membership.py
+++ 8 - Sets and Dictionaries/Module8/more_fun_with_collections/dict_membership.py
@@ -5,3 +5,9 @@ October 15, 2019
 Module 8, topic 2: Dictionaries
 """
 
+def in_dict(the_dict, element):
+    if (element in the_dict):
+        return True
+    else:
+        return False
+
--- 8 - Sets and Dictionaries/Module8/test/test_dict_membership.py
+++ 8 - Sets and Dictionaries/Module8/test/test_dict_membership.py
@@ -7,10 +7,18 @@ Module 8, topic 2: Dictionaries
 import unittest
 from dict_membership import in_dict
 
-class DictionaryTest(unittest.TestCase)
+class DictionaryTest(unittest.TestCase):
     def test_in_dict_true(self):
-        pass
+        a = {'A' : 90, 'B' : 80, 'C' : 70, 'D' : 20}
+        self.assertEqual(in_dict(a, 'A'), True)
+        self.assertEqual(in_dict(a, 'B'), True)
 
     def test_in_dict_false(self):
-        pass
+        b = {'E' : 90, 'F' : 80, 'G' : 70, 'H' : 20}
+        self.assertEqual(in_dict(b, 'E'), True)
+        self.assertEqual(in_dict(b, 'A'), False)
+        self.assertEqual(in_dict(b, 'B'), False)
+
+if __name__ == "__main__":
+    unittest.main()
 
