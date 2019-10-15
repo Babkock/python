036105130995bf948585f6b8706b468f9135985a
Tanner Babcock <babkock@gmail.com>
2019-10-15 18:27:26 -0500

    Dictionaries

---
+++ 8 - Sets and Dictionaries/Module8/more_fun_with_collections/dict_membership.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+October 15, 2019
+Module 8, topic 2: Dictionaries
+"""
+
---
+++ 8 - Sets and Dictionaries/Module8/test/test_dict_membership.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+October 15, 2019
+Module 8, topic 2: Dictionaries
+"""
+import unittest
+from dict_membership import in_dict
+
+class DictionaryTest(unittest.TestCase)
+    def test_in_dict_true(self):
+        pass
+
+    def test_in_dict_false(self):
+        pass
+
