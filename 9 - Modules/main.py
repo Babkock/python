3cec12713131dff685aaba059c586b241924a830
Tanner Babcock <babkock@gmail.com>
2019-10-20 18:31:08 -0500

    Module 9

---
+++ 9 - Modules/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import my_definitions as me
+
+me.print_dict({ 'a' : 5, 'b' : 4 })
+me.print_set(('hello', 'world', 5))
+
---
+++ 9 - Modules/my_definitions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+October 20, 2019
+Module 9, topic 1: Python Modules and Packages
+"""
+
+def greeting():
+    print("Hello, welcome to my Module!")
+
+def author():
+    print("This module written by Tanner Babcock")
+
+def print_dict(diction):
+    for key, value in diction.items():
+        print("{} - {}".format(key, value))
+
+def print_set(sett):
+    for key in enumerate(sett):
+        print("{}".format(key))
+
