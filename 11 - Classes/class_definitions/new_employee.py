a339fb08f570da15d6721b6c709c66f1ac2b6950
Tanner Babcock <babkock@gmail.com>
2019-11-06 19:24:06 -0600

    Working on topic 4: Inheritance and polymorphism

---
+++ 11 - Classes/class_definitions/new_employee.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+November 6, 2019
+Module 11, topic 4: Inheritance and Polymorphism
+"""
+import datetime
+
+class Employee:
+    # Constructor
+    def __init__(self, fname, lname, address, phone):
+        if (isinstance(lname, str) != True):
+            raise ValueError
+        if (isinstance(fname, str) != True):
+            raise ValueError
+        if (isinstance(address, str) != True):
+            raise ValueError
+        if (isinstance(phone, str) != True):
+            raise ValueError
+        #if (isinstance(start, datetime.datetime) != True) and (isinstance(start, datetime.date) != True):
+        #    raise ValueError
+
+        self._last_name = lname
+        self._first_name = fname
+        self._address = address
+        self._phone = phone
+
+    def set_last_name(self, lname):
+        if (isinstance(lname, str) != True):
+            raise ValueError
+        self._last_name = lname
+
+    def set_first_name(self, fname):
+        if (isinstance(fname, str) != True):
+            raise ValueError
+        self._first_name = fname
+
+    def set_address(self, addr):
+        if (isinstance(addr, str) != True):
+            raise ValueError
+        self._address = addr
+
+    def set_phone(self, phone):
+        if (isinstance(phone, str) != True):
+            raise ValueError
+        self._phone = phone
+
+    def display(self):
+        output = str(self._first_name) + " " + str(self._last_name) + "\n"
+        output += self._address + "\n"
+        if self._salaried == True:
+            output += "Salaried employee: ${}/year\n".format(self._salary)
+        else:
+            output += "Hourly employee: ${0:.2f}/hour\n".format(self._salary)
+        output += "Start date: " + str(self._start_date) + "\n"
+        return output
+
+# Driver
+address = "1 Infinite Loop\nCupertino, CA"
+# call the constructor
+emp = Employee("Steve", "Jobs", address, "212-222-2222", True, datetime.datetime.strptime("10-1-1985", "%m-%d-%Y"), 1000000)
+
+print(emp.display())    # display() returns a str
+del emp
+
