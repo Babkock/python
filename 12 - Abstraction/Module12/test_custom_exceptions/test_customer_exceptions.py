45e24be4bd674317e1f781deae1f347c02f57417
Tanner Babcock <babkock@gmail.com>
2019-11-12 21:40:30 -0600

    Completed Unit Tests for Custom Exceptions

--- 12 - Abstraction/Module12/test_custom_exceptions/test_customer_exceptions.py
+++ 12 - Abstraction/Module12/test_custom_exceptions/test_customer_exceptions.py
@@ -5,14 +5,36 @@ November 12, 2019
 Module 12, topic 2: Custom Exceptions
 """
 import unittest
-from customer_exceptions import Customer
-from customer_exceptions import valid_number
+from customer_exceptions import *
 
 class CustomExceptionsTest(unittest.TestCase):
     def setUp(self):
-        self.customer = Customer(1002, "Jonathan", "George", "555-444-3333")
+        self.customer = Customer(1002, "Boy", "George", "555-444-3333")
 
     def tearDown(self):
         del self.customer
 
+    def test_customer_str(self):
+        print(self.customer)
+
+    def test_customer_bad_customer_id(self):
+        with self.assertRaises(InvalidCustomerIdException):
+            bad = Customer(1, "Hello", "World", "555-444-3333")
+
+    def test_customer_bad_first_name(self):
+        with self.assertRaises(InvalidNameException):
+            bad = Customer(1002, ".", "World", "555-444-3333")
+
+    def test_customer_bad_last_name(self):
+        with self.assertRaises(InvalidNameException):
+            bad = Customer(1000, "Hello", ".", "444-333-5555")
+
+    def test_customer_bad_phone_number(self):
+        with self.assertRaises(InvalidPhoneNumberFormat):
+            badphone = "43-5"
+            self.assertEqual(valid_number(badphone), False)
+            bad = Customer(1003, "Hello", "World", badphone)
+
+if __name__ == "__main__":
+    unittest.main()
 
