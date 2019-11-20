e2ae6dd86238a91155876b800a128739fba11271
Tanner Babcock <babkock@gmail.com>
2019-11-19 19:14:47 -0600

    Starting Database query assignment

---
+++ 13 - Database/query.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Tanner Babcock
+November 19, 2019
+Module 13, topic 2: Connecting to a Database
+"""
+import sqlite3
+from sqlite3 import Error
+
+def create_connection():
+    pass
+
